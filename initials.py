35862d0ad4fc96d69ae62a300deeb10e66fdf7d7
enyonamlydia <enyonamlydia952@gmail.com>
2026-05-31 09:39:25 +0000

    Submitting my assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in large block letters.
+# Assignment Number: 1
+#
+# Name: Lydia Enyonam Avuworda
+# STUDENT ID:  2425402779
+# Email: 2425402779@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Lydia Enyonam Avuworda, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials L, E, A in large block letters, must be 12 char wide and 10 char high,
+    # each made of its own letter with dots to fill the empty spaces,
+    #consisting of 3 periods before and after each block letter,
+    # followed by a 2 asterisks repeated twice on the last two rows.
 
+    print()
+    print("...LEA")
+    print()
+
+    print("...LL..................EEEEEEEEEEEE............AAAA.........")
+    print("...LL..................EE....................AA....AA.......")
+    print("...LL..................EE...................AA......AA......")
+    print("...LL..................EE..................AA........AA.....")
+    print("...LL..................EEEEEEEE............AA........AA.....")
+    print("...LL..................EEEEEEEE............AAAAAAAAAAAA.....")
+    print("...LL..................EE..................AA........AA.....")
+    print("...LL..................EE..................AA........AA.....")
+    print("...LL.............**...EE.............**...AA........AA...**")
+    print("...LLLLLLLLLLLL...**...EEEEEEEEEEEE...**...AA........AA...**")
+
+    print()
 
 
 main()
